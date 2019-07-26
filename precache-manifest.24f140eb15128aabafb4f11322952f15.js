4abb4088b266bfac0b083d32acfdb6b5b48949db
eysong <>
2019-07-26 13:45:18 +0900

    Updates

---
+++ precache-manifest.24f140eb15128aabafb4f11322952f15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f768da3914897f1b02a99cf46343e2",
+    "url": "/test3/index.html"
+  },
+  {
+    "revision": "381ba596becd7e0613fe",
+    "url": "/test3/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test3/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "381ba596becd7e0613fe",
+    "url": "/test3/static/js/main.934af2ce.chunk.js"
+  },
+  {
+    "revision": "0f5abcbcf96eb39e736c",
+    "url": "/test3/static/js/runtime~main.ff6a7e7b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/test3/static/media/logo.ee7cd8ed.svg"
+  }
+]);
